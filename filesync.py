f4c574bb58dc2fe1d297231e80cd5ff4ce6e5037
Chris Engelhart <engleman@gmail.com>
2020-05-03 15:26:05 -0400

    Allow changing thresholds on CLI and workaround for ytdl issue

--- filesync.py
+++ filesync.py
@@ -1,9 +1,10 @@
 
 from pathlib import Path
-import os
+import time
+
 
 class DirPair:
-# TODO allow S3 or somesuch
+    # TODO allow S3 or somesuch
 
     def __init__(self, fromdir, todir):
         self.fromdir = Path(fromdir)
@@ -15,35 +16,32 @@ class DirPair:
         assert self.todir.is_dir()
         # TODO verify both dirs are writable
 
-    def xfernow(self, thresh=1048576, maxage > 60):
+    def xfernow(self, thresh=1048576, maxage=60):
         self.validate()
         # TODO make this recursive
-        for child in fromdir.iterdir():
+        for child in self.fromdir.iterdir():
             if not child.is_file() or child.is_symlink():
                 continue
             stats = child.stat()
             currtime = time.time()
             threshtime = currtime - maxage
             # TODO verify all paths writable
-            if stats.st_size < thresh or stats.st_atime > threshtime or stats.st_mtime > threshtime or stats.st_ctime > threshtime:
+            if (stats.st_size < thresh or stats.st_atime > threshtime or
+                    stats.st_mtime > threshtime or
+                    stats.st_ctime > threshtime):
                 continue
             # TODO accept already synced stuff
-            
-
-
-        
-
 
 
 class SyncingPair:
 
     def __init__(self, dirpair, path):
         self.dirpair = dirpair
-        
 
 
 def filestatus(fname):
+    pass
 
 
 def syncfile(frm, to):
-
+    pass
--- mvln.py
+++ mvln.py
@@ -18,7 +18,15 @@ def mvlns(src, dst, verbose=True):
     assert os.path.isfile(src)
     assert not os.path.islink(src)
     assert os.path.isdir(os.path.dirname(dst))
-    assert not os.path.lexists(dst)
+    if os.path.lexists(dst):
+        # Some annoying programs will amend but overwrite the link
+        # The following lets it still work
+        # TODO needs to be handled better
+        srcst = os.stat(src)
+        dstst = os.stat(dst)
+        assert srcst.st_size > dstst.st_size - 1024
+        assert srcst.st_size < dstst.st_size + 1024
+        assert srcst.st_mtime == dstst.st_mtime
     shutil.move(src, dst)
     # note - src and dst can be a little counter intuitive
     os.symlink(dst, src)
@@ -45,8 +53,12 @@ def _cli():
     parser = argparse.ArgumentParser(description='move and link')
     parser.add_argument('source', help='source directory')
     parser.add_argument('dest', help='target directory')
+    parser.add_argument('-a', '--age', type=int, default=1800,
+                        help='Min age in seconds')
+    parser.add_argument('-s', '--size', type=int, default=2097152,
+                        help='Min size in bytes')
     args = parser.parse_args()
-    moveold(args.source, args.dest)
+    moveold(args.source, args.dest, args.age, args.size)
 
 
 if __name__ == "__main__":
